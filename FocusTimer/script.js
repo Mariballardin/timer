d4c5f4ad89ebce0d5c9ccd9fac877a8f05adc350
Mariana Ferreira <96913788+Mariballardin@users.noreply.github.com>
2022-06-15 18:11:38 +0200

    changes JS

--- FocusTimer/script.js
+++ FocusTimer/script.js
@@ -1,3 +1,6 @@
+
+// Aqui estou usando o conceito de DOM para acessar objetos do meu HTML. DOM é uma programação event-driven - direcionada a eventos.
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonSoundOn = document.querySelector('.sound-on')
@@ -5,15 +8,41 @@ const buttonSoundOff = document.querySelector('.sound-off')
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector ('.seconds')
 
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.remove('hide')
+
+}
+
+function updateTimerDisplay (minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+
+}
+
 function countdown() {
 
   setTimeout(function() {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+    secondsDisplay.textContent = "00"
+
+    if (minutes <= 0) {
+
+      resetControls()
+
+      return
+    }
     
     if (seconds <= 0) {
       seconds = 60
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
     }
-    secondsDisplay.textContent = seconds - 1
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
 
     countdown()
   }, 1000 )
@@ -47,10 +76,8 @@ buttonPause.addEventListener('click', function () {
 
 
 buttonStop.addEventListener('click', function() {
-  buttonPlay.classList.remove('hide')
-  buttonStop.classList.add('hide')
-  buttonPause.classList.add('hide')
-  buttonSet.classList.remove('hide')
+ 
+      resetControls()
   })
 
   buttonSoundOn.addEventListener('click', function (){
@@ -67,6 +94,6 @@ buttonStop.addEventListener('click', function() {
 
   buttonSet.addEventListener('click', function() {
     minutes = prompt('Quantos minutos você gostaria de cronometrar?')
-    minutesDisplay.textContent = minutes
+    updateTimerDisplay(minutes, 0)
 
   })
